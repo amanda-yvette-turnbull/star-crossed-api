8e6b65294bfa31bfe98d2c435481aae3017101c8
Finn Connell <finn.bc@protonmail.com>
2023-07-04 09:33:39 +1200

    updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 # StarSigns
-StarSign.create(date_range: '3/21-4/19', name: 'Aries', blurb: 'Angry one', image: '/images/image', default_compatibility: '[3,1,4,2,5,1,3,1,5,2,4,1]')
-StarSign.create(date_range: '4/20-5/20', name: 'Taurus', blurb: 'Cow Moo', image: '/images/image', default_compatibility: '[2,3,1,4,2,5,1,3,2,5,2,4]')
-StarSign.create(date_range: '5/21-6/20', name: 'Gemini', blurb: 'Twinning', image: '/images/image', default_compatibility: '[4,2,3,2,4,2,5,2,3,1,5,2]')
-StarSign.create(date_range: '6/21-7/22', name: 'Cancer', blurb: 'Crabby much?', image: '/images/image', default_compatibility: '[2,1,2,5,4,3,1,5,4,1,3,2]')
-StarSign.create(date_range: '7/23-8/22', name: 'Leo', blurb: 'Rawr XD', image: '/images/image', default_compatibility: '[5,4,3,2,1,3,2,1,5,4,1,2]')
-StarSign.create(date_range: '8/23-9/22', name: 'Virgo', blurb: 'Femcel energy', image: '/images/image', default_compatibility: '[3,2,4,5,4,1,3,1,2,3,5,1]')
-StarSign.create(date_range: '9/23-10/22', name: 'Libra', blurb: 'Blah, in all things', image: '/images/image', default_compatibility: '[3,4,5,3,2,4,1,5,1,2,3,1]')
+StarSign.create(date_range: '03/21-04/19', name: 'Aries', blurb: 'Angry one', image: '/images/image', default_compatibility: '[3,1,4,2,5,1,3,1,5,2,4,1]')
+StarSign.create(date_range: '04/20-05/20', name: 'Taurus', blurb: 'Cow Moo', image: '/images/image', default_compatibility: '[2,3,1,4,2,5,1,3,2,5,2,4]')
+StarSign.create(date_range: '05/21-06/20', name: 'Gemini', blurb: 'Twinning', image: '/images/image', default_compatibility: '[4,2,3,2,4,2,5,2,3,1,5,2]')
+StarSign.create(date_range: '06/21-07/22', name: 'Cancer', blurb: 'Crabby much?', image: '/images/image', default_compatibility: '[2,1,2,5,4,3,1,5,4,1,3,2]')
+StarSign.create(date_range: '07/23-08/22', name: 'Leo', blurb: 'Rawr XD', image: '/images/image', default_compatibility: '[5,4,3,2,1,3,2,1,5,4,1,2]')
+StarSign.create(date_range: '08/23-09/22', name: 'Virgo', blurb: 'Femcel energy', image: '/images/image', default_compatibility: '[3,2,4,5,4,1,3,1,2,3,5,1]')
+StarSign.create(date_range: '09/23-10/22', name: 'Libra', blurb: 'Blah, in all things', image: '/images/image', default_compatibility: '[3,4,5,3,2,4,1,5,1,2,3,1]')
 StarSign.create(date_range: '10/23-11/21', name: 'Scorpio', blurb: 'Toxic backstabber', image: '/images/image', default_compatibility: '[1,2,3,5,4,3,5,1,4,1,2,1]')
 StarSign.create(date_range: '11/22-12/21', name: 'Sagittarius', blurb: 'Horse Girl', image: '/images/image', default_compatibility: '[5,4,2,1,3,1,4,2,5,1,3,1]')
-StarSign.create(date_range: '12/22-1/19', name: 'Capricorn', blurb: 'Nutso!', image: '/images/image', default_compatibility: '[2,1,2,4,5,3,2,5,1,4,1,3]')
-StarSign.create(date_range: '1/20-2/18', name: 'Aquarius', blurb: 'Wishy-Washy', image: '/images/image', default_compatibility: '[1,2,4,3,2,1,4,5,3,5,1,2]')
-StarSign.create(date_range: '2/19-3/20', name: 'Pisces', blurb: 'Gonna Cry?', image: '/images/image', default_compatibility: '[4,3,2,1,3,5,2,1,4,3,5,1]')
+StarSign.create(date_range: '12/22-01/19', name: 'Capricorn', blurb: 'Nutso!', image: '/images/image', default_compatibility: '[2,1,2,4,5,3,2,5,1,4,1,3]')
+StarSign.create(date_range: '01/20-02/18', name: 'Aquarius', blurb: 'Wishy-Washy', image: '/images/image', default_compatibility: '[1,2,4,3,2,1,4,5,3,5,1,2]')
+StarSign.create(date_range: '02/19-03/20', name: 'Pisces', blurb: 'Gonna Cry?', image: '/images/image', default_compatibility: '[4,3,2,1,3,5,2,1,4,3,5,1]')
 
 # Users
 User.create(auth0_id: '1', name: 'Oscar', age: 25, email: 'oscar@mail.com', gender: 'masc', preference: '[masc, femme, them]', bio: 'great dancer', birthday: '1/3', image: 'images/user', star_sign_id: '11', matches: '', compatibility: '[1,2,4,3,2,1,4,5,3,5,1,2]')
